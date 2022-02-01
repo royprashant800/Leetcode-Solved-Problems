99d28b5d84094bc8a9a8e45458d17b5323d4f95b
Prashant Kumar <68124619+royprashant800@users.noreply.github.com>
2022-02-01 11:21:13 +0530

    Added solution - LeetHub

---
+++ IPL 2021 - Match Day 2 - GFG/ipl-2021-match-day-2.java
@@ -0,0 +1,61 @@
+// { Driver Code Starts
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+class Main {
+    public static void main(String args[]) {
+        // taking input using class Scanner
+        Scanner sc = new Scanner(System.in);
+
+        // taking total count of testcases
+        int t = sc.nextInt();
+
+        while (t-- > 0) {
+            // taking total number of elements
+            int n = sc.nextInt();
+
+            // taking size of subArrays
+            int k = sc.nextInt();
+
+            // Declaring and Intializing an array of size n
+            int arr[] = new int[n];
+
+            // adding all the elements to the array
+            for (int i = 0; i < n; i++) {
+                arr[i] = sc.nextInt();
+            }
+
+            // Calling the method max_of_subarrays of class solve
+            // and storing the result in an ArrayList
+            ArrayList<Integer> res = new Solution().max_of_subarrays(arr, n, k);
+
+            // printing the elements of the ArrayList
+            for (int i = 0; i < res.size(); i++)
+                System.out.print(res.get(i) + " ");
+            System.out.println();
+        }
+    }
+}// } Driver Code Ends
+
+
+class Solution {
+    static ArrayList<Integer> max_of_subarrays(int nums[], int n, int k) {
+		ArrayList<Integer> res = new ArrayList<>();
+		Deque<Integer> dq = new LinkedList<>();
+		int ri=0;
+		
+		for(int i=0; i<n; i++) {
+			if(!dq.isEmpty() && dq.peek() == i - k) 
+				dq.poll();
+			
+			while(!dq.isEmpty() && nums[dq.peekLast()] < nums[i])
+				dq.pollLast();
+			
+			dq.offer(i);
+			if(i >= k - 1)
+				res.add(nums[dq.peek()]);
+		}
+		return res;
+    }
+}
