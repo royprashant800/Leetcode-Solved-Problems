187c395e93590d857153ba11b6e63b5486bd5a3e
Prashant Kumar <68124619+royprashant800@users.noreply.github.com>
2022-03-11 23:30:49 +0530

    Time: 1 ms (46.27%), Space: 41.3 MB (35.89%) - LeetHub

--- 1359-count-all-valid-pickup-and-delivery-options/1359-count-all-valid-pickup-and-delivery-options.java
+++ 1359-count-all-valid-pickup-and-delivery-options/1359-count-all-valid-pickup-and-delivery-options.java
@@ -1,20 +1,13 @@
 class Solution {
-     public int countOrders(int n) {
-        if(n==1) return 1;
-        else if(n==2) return 6;
-        int[] dp = new int[n+1];
-        dp[1] = 1;
-        dp[2] = 6;
-        
-        long mod = 1000000007;
-        for(int i=3;i<=n;i++)
-        {
-            long k = (long) (i*2 -1);    // k = total number of places in n-1
-            long each = (k*(k+1))/2;     //total possibilities in each case of n-1
-            long total = each * (long)dp[i-1];    //total possibilities for n 
-            dp[i] = (int) (total%mod);
+     public int countOrders(int n) {        
+        int mod = 1000000007;
+        long res = 1;
+        for(int i = 1; i <= n; i++) {
+            res *= i;
+            res %= mod;
+            res *= (2 * i - 1);
+            res %= mod;
         }
-        
-        return dp[n];
+        return (int)(res % mod);
     }
 }
