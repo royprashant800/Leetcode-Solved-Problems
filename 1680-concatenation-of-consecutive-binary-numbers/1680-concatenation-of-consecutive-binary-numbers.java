58ac78c832430157517e6e5a20cc8d0537abaead
Prashant Kumar <68124619+royprashant800@users.noreply.github.com>
2022-04-12 22:14:46 +0530

    Time: 93 ms (74.00%), Space: 41.7 MB (38.00%) - LeetHub

--- 1680-concatenation-of-consecutive-binary-numbers/1680-concatenation-of-consecutive-binary-numbers.java
+++ 1680-concatenation-of-consecutive-binary-numbers/1680-concatenation-of-consecutive-binary-numbers.java
@@ -2,11 +2,11 @@ class Solution {
     public int concatenatedBinary(int n) {
         long res = 0;
         int mod = 1000000007;
+        int size = 0;
         
-        for(int i = 1; i <= n; i++) {
-            String bs = Integer.toBinaryString(i);
-            res = (res << bs.length()) % mod;
-            res = (res + i) % mod;
+        for(int i = 1; i <= n; i++) {;
+            if((i & (i - 1)) == 0) size++;
+            res = (res << size | i) % mod;
         }
         return (int)res;
     }
