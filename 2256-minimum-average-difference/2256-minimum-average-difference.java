4736e24e316f42a4a30c9b5f15be67bf2c8eefa4
Prashant Kumar <68124619+royprashant800@users.noreply.github.com>
2022-04-30 22:29:56 +0530

    Time: 19 ms (28.57%), Space: 75.2 MB (71.43%) - LeetHub

--- 2256-minimum-average-difference/2256-minimum-average-difference.java
+++ 2256-minimum-average-difference/2256-minimum-average-difference.java
@@ -1,39 +1,19 @@
 class Solution {
     public int minimumAverageDifference(int[] nums) {
-        int n = nums.length;
-        if(n == 1) return 0;
-        int[] res = new int[n];
+        int n = nums.length, res = 0;
         
-        long sum1 = nums[0], sum2 = 0;
-        for(int i = 1; i < n; i++) sum2 += nums[i];
+        long min = Integer.MAX_VALUE, sum = 0, leftSum = 0, rightSum = 0;
+        for(int num : nums) sum += num;
         
         for(int i = 0; i < n; i++) {
-            long left, right;
-            
-            if(i == 0) {
-                left = sum1;
-                right = sum2 / (n - 1 - i);
-                res[i] = (int)Math.abs(left - right);
-            } else if(i == n - 1) {
-                sum1 += (long)nums[i];
-                left = sum1 / (i + 1);
-                res[i] = (int)left;
-            } else {
-                sum1 += (long)nums[i];
-                sum2 -= (long)nums[i];
-                left = sum1 / (i + 1);
-                right = sum2 / (n - 1 - i);
-                res[i] = (int)Math.abs(left - right);
+            leftSum += nums[i];
+            rightSum = sum - leftSum;
+            long diff = Math.abs(leftSum /(i + 1) - (n - i == 1 ? 0 : rightSum / (n - 1 - i)));
+            if(diff < min) {
+                min = diff;
+                res = i;
             }
         }
-        
-        int index  = 0, val = Integer.MAX_VALUE;
-        for(int i = 0; i < n; i++) {
-            if(res[i] < val) {
-                index = i;
-                val = res[i];
-            }
-        }
-        return index;
+        return res;
     }
 }
