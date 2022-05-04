0a982d46a086934283d983fd13d71980776f7a4b
Prashant Kumar <68124619+royprashant800@users.noreply.github.com>
2022-05-04 23:12:24 +0530

    Time: 56 ms (40.96%), Space: 155.4 MB (49.40%) - LeetHub

--- 1968-array-with-elements-not-equal-to-average-of-neighbors/1968-array-with-elements-not-equal-to-average-of-neighbors.java
+++ 1968-array-with-elements-not-equal-to-average-of-neighbors/1968-array-with-elements-not-equal-to-average-of-neighbors.java
@@ -1,23 +1,11 @@
 class Solution {
-    public int[] rearrangeArray(int[] arr) {
-        int n = arr.length;
-        
-        for(int i = 1; i < n - 1; i++) {
-            double num = (double)(arr[i - 1] + arr[i + 1]) / 2;
-            if(arr[i] == num) {
-                int temp = arr[i];
-                arr[i] = arr[i + 1];
-                arr[i + 1] = temp;
-            }
+    public int[] rearrangeArray(int[] nums) {
+        Arrays.sort(nums);
+        for(int i = 1; i < nums.length; i += 2) {
+            int temp = nums[i];
+            nums[i] = nums[i - 1];
+            nums[i - 1] = temp;
         }
-        for(int i = n - 2; i >= 1; i--) {
-            double num = (double)(arr[i - 1] + arr[i + 1]) / 2;
-            if(arr[i] == num) {
-                int temp = arr[i - 1];
-                arr[i - 1] = arr[i];
-                arr[i] = temp;
-            }
-        }
-        return arr;
+        return nums;
     }
 }
