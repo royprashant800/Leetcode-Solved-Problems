feb1dd0c4a8f68ee558e2f85d7837e5c38bc1109
Prashant Kumar <68124619+royprashant800@users.noreply.github.com>
2022-05-15 11:46:53 +0530

    Time: 2 ms (94.34%), Space: 55.9 MB (52.76%) - LeetHub

--- 1302-deepest-leaves-sum/1302-deepest-leaves-sum.java
+++ 1302-deepest-leaves-sum/1302-deepest-leaves-sum.java
@@ -1,28 +1,22 @@
 class Solution {
-    int level;
+    int maxLevel;
     int sum = 0;
     public int deepestLeavesSum(TreeNode root) {
-        level = height(root);
-        int[] t = new int[1];
-        sum(root, t);
+        sum(root, 1);
         return sum;
     }
-    private int height(TreeNode root) {
-        if(root == null) {
-            return 0;
-        }    
-        
-        return 1 + Math.max(height(root.left), height(root.right));
-    }
-    private void sum(TreeNode root, int[] temp) {
+    private void sum(TreeNode root, int curr) {
         if(root == null) 
             return;
         
-        temp[0]++;
-        if(temp[0] == level)
+        if(curr > maxLevel) {
+            maxLevel = curr;
+            sum = 0;
+        }
+        if(curr == maxLevel)
             sum += root.val;
-        sum(root.left, temp);
-        sum(root.right, temp);
-        temp[0]--;
+        
+        sum(root.left, curr + 1);
+        sum(root.right, curr + 1);
     } 
 }
