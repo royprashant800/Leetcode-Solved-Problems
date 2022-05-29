1bf7fff265b194c0fa5c75e9e577d2107a367d39
Prashant Kumar <68124619+royprashant800@users.noreply.github.com>
2022-05-29 22:41:32 +0530

    Time: 13 ms (62.98%), Space: 48.1 MB (37.57%) - LeetHub

--- 318-maximum-product-of-word-lengths/318-maximum-product-of-word-lengths.java
+++ 318-maximum-product-of-word-lengths/318-maximum-product-of-word-lengths.java
@@ -1,29 +1,18 @@
 class Solution {
     public int maxProduct(String[] words) {
         int n = words.length;
-        int res = 0;
+        int[] bitmask = new int[n];
         
-        for(int i = 0; i < n; i++) {
-            String s1 = words[i];
-            for(int j = i + 1; j < n; j++) {
-                String s2 = words[j];
-                int[] f1 = new int[26];
-                int[] f2 = new int[26];
-                
-                for(int k = 0; k < s1.length(); k++)
-                    f1[s1.charAt(k) - 'a']++;
-                
-                boolean flag = true;
-                for(int k = 0; k < s2.length(); k++) 
-                    f2[s2.charAt(k) - 'a']++;
-                
-                for(int k=0; k<26; k++) {
-                    if(f1[k] > 0 && f2[k] > 0)
-                        flag = false;
-                }
-                if(flag) res = Math.max(res, s1.length() * s2.length());
-            }
-        }
+        for(int i = 0; i < n; i++) 
+            for(char c : words[i].toCharArray())
+                bitmask[i] |= (1 << (c - 'a'));
+        
+        int res = 0;
+        for(int i = 0; i < n; i++) 
+            for(int j = i + 1; j < n; j++)
+                if((bitmask[i] & bitmask[j]) == 0)
+                    res = Math.max(res, words[i].length() * words[j].length());
+        
         return res;
     }
 }
