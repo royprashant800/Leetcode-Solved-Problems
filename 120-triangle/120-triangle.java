46f38f248d045473e51a3e6bff9a33e11fe196cb
Prashant Kumar <68124619+royprashant800@users.noreply.github.com>
2022-06-13 19:08:04 +0530

    Time: 12 ms (11.50%), Space: 43.8 MB (61.91%) - LeetHub

---
+++ 120-triangle/120-triangle.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int minimumTotal(List<List<Integer>> triangle) {
+        int n = triangle.size();
+        
+        for(int i=1; i<n; i++) {
+            for(int j=0; j<triangle.get(i).size(); j++) {
+                int sum = 0;
+                if(j == 0)
+                    sum = triangle.get(i).get(j) + triangle.get(i - 1).get(j);
+                else if(j == triangle.get(i).size() - 1)
+                    sum = triangle.get(i).get(j) + triangle.get(i - 1).get(triangle.get(i - 1).size() - 1);
+                else {
+                    int min = Math.min(triangle.get(i - 1).get(j), triangle.get(i - 1).get(j - 1));
+                    sum = min + triangle.get(i).get(j);
+                }
+                triangle.get(i).set(j, sum);
+            }
+        }
+        return Collections.min(triangle.get(n - 1));
+    }
+}
