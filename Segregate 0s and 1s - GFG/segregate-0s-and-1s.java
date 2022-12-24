16184a80b6b885f9b0c0bfcd48fa99b75b6baf9a
Prashant Kumar <68124619+royprashant800@users.noreply.github.com>
2022-12-24 16:23:23 +0530

    Added solution - LeetHub

---
+++ Segregate 0s and 1s - GFG/segregate-0s-and-1s.java
@@ -0,0 +1,51 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine().trim());
+        while (tc-- > 0) {
+            String[] inputLine;
+            inputLine = br.readLine().trim().split(" ");
+            int n = Integer.parseInt(inputLine[0]);
+            int[] arr = new int[n];
+            inputLine = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                arr[i] = Integer.parseInt(inputLine[i]);
+            }
+            new Solution().segregate0and1(arr, n);
+            for (int i = 0; i < n; i++) {
+                System.out.print(arr[i] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
+
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution {
+    void segregate0and1(int[] nums, int n) {
+        int i = 0, j = nums.length - 1;
+		while(i <= j) {
+			if(nums[i] == 0) 
+				i++;
+			else if(nums[i] == 1) {
+				int temp = nums[i];
+				nums[i] = nums[j];
+				nums[j] = temp;
+				j--;
+			}
+		}
+    }
+
+}
