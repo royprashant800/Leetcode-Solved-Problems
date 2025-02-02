f300ca1b05217525d885b3eaf29bd4bc24dcefd6
Prashant Kumar <68124619+royprashant800@users.noreply.github.com>
2025-02-02 19:07:13 +0530

    Time: 3 ms (4.29%), Space: 42.9 MB (8.39%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -1,30 +1,25 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        int i = 0, j = 0, k = 0;
+        int i = 0, j = 0;
         while(i < m && j < n) {
-            if(nums1[i] <= nums2[j]) nums1[k++] = nums1[i++];
+            if(nums1[i] <= nums2[j]) 
+                i++;
             else {
                 int temp = nums1[i];
                 nums1[i] = nums2[j];
-                nums2[j] = temp; 
-                i++;k++;
-                int l = 1;
-                while(l < n && nums2[l] < nums2[l - 1]) {
-                    swap(nums2, l, l - 1);
-                    l++;
+                nums2[j] = temp;
+                i++;
+                for(int k=1; k<n; k++) {
+                    if(nums2[k] < nums2[k-1]) {
+                        int num = nums2[k];
+                        nums2[k] = nums2[k - 1];
+                        nums2[k - 1] = num;
+                    }
                 }
             }
         }
-        if(i < m) {
-            while(i < m) nums1[k++] = nums1[i++];
-        } 
-        if(j < n) {
-            while(j < n) nums1[k++] = nums2[j++];
-        } 
-    }
-    private void swap(int[] nums, int i, int j) {
-        int temp = nums[i];
-        nums[i] = nums[j];
-        nums[j] = temp;
+        while(j < n) {
+            nums1[i++] = nums2[j++];
+        }
     }
 }
