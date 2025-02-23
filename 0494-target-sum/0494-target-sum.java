70e2707ebf564dd9a471a2d4e3488f34cd074318
Prashant Kumar <68124619+royprashant800@users.noreply.github.com>
2025-02-23 19:33:16 +0530

    Time: 4 ms (93.67%), Space: 43.3 MB (41.97%) - LeetHub

--- 0494-target-sum/0494-target-sum.java
+++ 0494-target-sum/0494-target-sum.java
@@ -1,20 +1,24 @@
 class Solution {
     public int findTargetSumWays(int[] nums, int target) {
-        int sum = 0;
+        int sum = 0, n = nums.length;
         for(int num : nums) sum += num;
         if(sum - target < 0 || (sum - target) % 2 == 1) return 0;
-        return solve(nums, nums.length - 1, (sum - target) / 2);
+        int s2 = (sum - target) / 2;
+        Integer[][] dp = new Integer[n][s2 + 1];
+        return solve(nums, n - 1, s2, dp);
     }
-    private int solve(int[] nums, int n, int target) {
+    private int solve(int[] nums, int n, int target, Integer[][] dp) {
         if(n == 0) {
             if(target == 0 && nums[0] == 0) return 2;
             if(target == 0 || target == nums[0]) return 1;
             return 0;
         }
 
-        int notTake = solve(nums, n - 1, target);
+        if(dp[n][target] != null) return dp[n][target];
+
+        int notTake = solve(nums, n - 1, target, dp);
         int take = 0;
-        if(nums[n] <= target) take = solve(nums, n - 1, target - nums[n]);
-        return notTake + take;
+        if(nums[n] <= target) take = solve(nums, n - 1, target - nums[n], dp);
+        return dp[n][target] = notTake + take;
     }
 }
