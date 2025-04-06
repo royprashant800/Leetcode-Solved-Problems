4892216dcb4055510bc6b6efed1c4c71eab678bf
Prashant Kumar <68124619+royprashant800@users.noreply.github.com>
2025-04-06 23:34:48 +0530

    Time: 27 ms (9.64%), Space: 43.4 MB (33.26%) - LeetHub

---
+++ 0368-largest-divisible-subset/0368-largest-divisible-subset.java
@@ -0,0 +1,25 @@
+class Solution {
+    List<Integer> res;
+    int[] mem;
+    public List<Integer> largestDivisibleSubset(int[] nums) {
+        res = new ArrayList<>();
+        mem = new int[nums.length];
+        Arrays.fill(mem, -1);
+        Arrays.sort(nums);
+        helper(nums, 0, new ArrayList<>(), 1);
+        return res; 
+    }
+    private void helper(int[] nums, int index, List<Integer> curr, int prev) {
+        if(curr.size() > res.size())
+            res = new ArrayList<>(curr);
+        
+        for(int i=index; i<nums.length; i++) {
+            if(curr.size() > mem[i] && nums[i] % prev == 0) {
+                mem[i] = curr.size();
+                curr.add(nums[i]);
+                helper(nums, i + 1, curr, nums[i]);
+                curr.remove(curr.size() - 1);
+            }
+        }
+    }
+}
